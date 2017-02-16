ebc30976f56c595dc4b7196a07bc1d67803cc640
ruizjme <hello@ruizjaime.com>
2017-02-16 19:12:36 +1100

    email icon

--- js/piechart.js
+++ js/piechart.js
@@ -35,5 +35,23 @@
 
         // Instantiate and draw our chart, passing in some options.
         var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+
+
+        ////////////////////////////
+        // The select handler. Call the chart's getSelection() method
+        function selectHandler() {
+          var selectedItem = chart.getSelection()[0];
+          if (selectedItem) {
+            var value = data.getValue(selectedItem.row, selectedItem.column);
+            alert('The user selected ' + value);
+          }
+        }
+
+        // Listen for the 'select' event, and call my function selectHandler() when
+        // the user selects something on the chart.
+        google.visualization.events.addListener(chart, 'select', selectHandler);
+        /////////////////////////
+
+
         chart.draw(data, options);
       }
